4734b1267a665cd0e615ede8116ab2a3eb340675
Danilian Akhmedzianov <hugefast@gmail.com>
2021-07-04 01:12:47 +0300

    add exclude subdomains feature

--- app/src/main/java/tk/pokatomnik/suspicious/CustomFragments/DomainCaptureFragment.java
+++ app/src/main/java/tk/pokatomnik/suspicious/CustomFragments/DomainCaptureFragment.java
@@ -1,10 +1,26 @@
 package tk.pokatomnik.suspicious.CustomFragments;
 
 import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
 
 public abstract class DomainCaptureFragment extends CaptureFragment<String> {
+    // TODO add more if needed
+    private static final List<String> excludeSubdomains = Arrays.asList("www", "m");
+
     @Override
     protected String map(String input) throws Exception {
-        return new URI(input).getHost();
+        final String host = new URI(input).getHost();
+        final String[] hostPartsAsList = host.split("\\.");
+
+        if (hostPartsAsList.length <= 1) {
+            return host;
+        }
+
+        if (excludeSubdomains.contains(hostPartsAsList[0])) {
+            return host.replace(hostPartsAsList[0], "");
+        }
+
+        return host;
     }
 }
